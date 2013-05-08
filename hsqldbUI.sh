a0133cf43c68e43f2b299026e7edf54a427f225a
woofgl <woofgl@gmail.com>
2013-05-08 14:01:58 +0800

    . add importdata.sh and change some style

--- hsqldbUI.sh
+++
@@ -1,1 +0,0 @@
-java -cp ./target/sampleBookmarks/WEB-INF/lib/hsqldb-2.2.9.jar org.hsqldb.util.DatabaseManagerSwing
---
+++ importdata.sh
@@ -0,0 +1,2 @@
+psql -U postgres -d xpsearchliang_db -w postgres -h localhost -f /home/lwf/mywork/XpSearchLiang/src/main/resources/sql/00_init.sql
+psql -U postgres -d xpsearchliang_db -w postgres -h localhost -f /home/lwf/mywork/XpSearchLiang/src/main/resources/sql/01_seeddata.sql
