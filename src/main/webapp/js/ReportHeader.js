51ce6eb892bf0fe2bdf11d16826513968cde103b
woofgl <woofgl@gmail.com>
2013-05-23 18:17:04 +0800

    . remove unuse code

--- src/main/webapp/js/ReportHeader.js
+++ src/main/webapp/js/ReportHeader.js
@@ -51,74 +51,7 @@
                         view.$el.trigger("DO_SET_ROOT", root);
                     }
 				},
-            	
-            	"btap;.nav li.nav-menu":function(e){
-            		var view = this;
-            		var $e = view.$el;
-            		var $li = $(e.currentTarget);
-            		$e.find("li.nav-menu").removeClass("active");
-            		$li.addClass("active");
-            		var menu = $li.attr("data-nav");
-            		
-            		if(menu == "D3JS Demo"){
-            			$e.find("li.easeljsPart").hide();
-            		  	$e.find("li.d3jsPart").show();
-                		$e.find("li.fabricjsPart").hide();
-                		brite.display("D3JSContactCluster");
-                		$e.find("li.nav-item").removeClass("active");
-                		$e.find("li.nav-item[data-nav='D3JSContactCluster']").addClass("active");
-                		
-                		//hide the ControlBar
-                		$e.find(".ControlBar").hide();
-            		}else if(menu == "EaselJS Demo"){
-            		  	$e.find("li.easeljsPart").show();
-            		  	$e.find("li.d3jsPart").hide();
-                		$e.find("li.fabricjsPart").hide();
-                		brite.display("EaselJSForceClusterSlider");
-                		$e.find("li.nav-item").removeClass("active");
-                		$e.find("li.nav-item[data-nav='EaselJSForceClusterSlider']").addClass("active");
-                		
-                		//show the ControlBar
-                		$e.find(".ControlBar").show();
-            		}else if(menu == "FabricJS Demo"){
-            		  	$e.find("li.easeljsPart").hide();
-            		  	$e.find("li.d3jsPart").hide();
-                		$e.find("li.fabricjsPart").show();
-                		brite.display("FabricJSContactCluster");
-                		$e.find("li.nav-item").removeClass("active");
-                		$e.find("li.nav-item[data-nav='FabricJSContactCluster']").addClass("active");
-                		
-                		//hide the ControlBar
-                		$e.find(".ControlBar").hide();
-            		}
-            		
-            		$li.closest(".dropdown").find(".dropDownTitle").html(menu);
-            	},
-            	
-            	"btap;.nav li.nav-item":function(e){
-            		var view = this;
-            		var $e = view.$el;
-            		var $li = $(e.currentTarget);
-            		$e.find("li.nav-item").removeClass("active");
-            		$li.addClass("active");
-            		var menu = $li.attr("data-nav");
-            		
-            		if(menu == "UserWeight"){
-            		  	brite.display("UserWeight");
-            		}else if(menu == "UserWeightD3"){
-            		  	brite.display("UserWeightD3");
-            		}else if(menu == "UserWeightD3Cluster"){
-            		  	brite.display("UserWeightD3Cluster");
-            		}else if(menu == "D3JSContactCluster"){
-            		  	brite.display("D3JSContactCluster");
-            		}else if(menu == "EaselJSForceClusterSlider"){
-            		  	brite.display("EaselJSForceClusterSlider");
-            		}else if(menu == "EaselJSForceClusterSlider2"){
-            		  	brite.display("EaselJSForceClusterSlider2");
-            		}else if(menu == "FabricJSContactCluster"){
-            		  	brite.display("FabricJSContactCluster");
-            		}
-            	}
+
             }
         });
         
